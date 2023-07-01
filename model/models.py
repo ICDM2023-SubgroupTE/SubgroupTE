34fb5255c60624a3cbb410aedd2df4bb6eaa741a
ICDM2023-SubgroupTE <54523717+ICDM2023-SubgroupTE@users.noreply.github.com>
2023-07-01 13:46:53 -0400

    Update models.py

--- model/models.py
+++ model/models.py
@@ -20,30 +20,27 @@ class SubgroupTE(nn.Module):
         super(SubgroupTE, self).__init__()
         
         self.input_dim = config['input_dim']
-        emb_dim = config['emb_dim']
-        out_dim = config['out_dim']
-        n_layer = config['n_layers']
+        self.emb_dim = config['emb_dim']
+        self.out_dim = config['out_dim']
+        self.n_layer = config['n_layers']
+        self.nhead = 5
+
+        self.criterion = partial(SubgroupTE_loss, alpha=config['alpha'], beta=config['beta'], gamma=config['gamma'])
         self.n_clusters = config['n_clusters']
-        nhead = 5
-        alpha, beta, gamma = config['alpha'],config['beta'],config['gamma']
-        self.w = 1
-        self.criterion = partial(SubgroupTE_loss, alpha=alpha, beta=beta, gamma=gamma)
-        
+        self.assignment = ClusterAssignment(self.n_clusters, config['init'])        
+                
         self.embedding = nn.Sequential(
             nn.Linear(self.input_dim, self.input_dim), nn.ReLU()
             )
-            
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.input_dim, nhead=nhead, batch_first=True, dim_feedforward=emb_dim) 
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=n_layer)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.input_dim, nhead=self.nhead, batch_first=True, dim_feedforward=self.emb_dim) 
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=self.n_layer)
         self.init_treat_out = nn.Linear(self.input_dim, 1)
         self.init_control_out = nn.Linear(self.input_dim, 1)
-        
-        self.assignment = ClusterAssignment(self.n_clusters, self.w)
 
         outcomemodel = nn.Sequential(
-            nn.Linear(self.input_dim+self.n_clusters, out_dim), nn.ReLU(),
-            nn.Linear(out_dim, out_dim), nn.ReLU(),
-            nn.Linear(out_dim, 1)
+            nn.Linear(self.input_dim+self.n_clusters, self.out_dim), nn.ReLU(),
+            nn.Linear(self.out_dim, self.out_dim), nn.ReLU(),
+            nn.Linear(self.out_dim, 1)
             ) 
         
         self.control_out = deepcopy(outcomemodel)
@@ -51,15 +48,15 @@ class SubgroupTE(nn.Module):
         self.propensity = nn.Sequential(
             nn.Linear(self.input_dim+self.n_clusters, 1), nn.Sigmoid()
             )
-    
-    def get_features(self, x): #in: (batch, feature)
+
+    def get_features(self, x): # in: (batch, feature)
         n_samples = x.size(0)
         x = self.embedding(x)
         if len(x.shape) == 2:
-            x = x.unsqueeze(1) #in: (batch, seq=1, feature)
+            x = x.unsqueeze(1) # out: (batch, seq=1, feature)
         features = self.transformer_encoder(x)[:,-1]
-        y1_pred_init = self.init_treat_out(features)
-        y0_pred_init = self.init_control_out(features)
+        y1_pred_init = self.init_treat_out(features)   # out: (batch, 1)
+        y0_pred_init = self.init_control_out(features) # out: (batch, 1)
         return features, y0_pred_init, y1_pred_init
 
     def subgrouping(self, x):
@@ -77,8 +74,8 @@ class SubgroupTE(nn.Module):
         loss = self.criterion(y, t, t_pred, y0_pred, y1_pred, y0_pred_init, y1_pred_init)
         return loss, y0_pred, y1_pred, t_pred, clusters
     
-    def update_centers(self, x, x_lengths): 
-        _, y0_pred_init, y1_pred_init = self.get_features(x, x_lengths)
+    def update_centers(self, x): 
+        _, y0_pred_init, y1_pred_init = self.get_features(x)
         te = y1_pred_init - y0_pred_init
         self.assignment._adjust_centers(te)
         
